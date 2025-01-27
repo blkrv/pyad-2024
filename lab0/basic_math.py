08e915fe762ee9b771e20feb985e69f5fe15bc70
blkrv <90620886+blkrv@users.noreply.github.com>
2025-01-27 22:19:56 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -10,6 +10,20 @@ def matrix_multiplication(matrix_a, matrix_b):
     # put your code here
     pass
 
+    if len(matrix_a[0]) != len(matrix_b):
+        raise ValueError("Число столбцов первой матрицы должно быть равно числу строк второй матрицы.")
+
+    n = len(matrix_a)
+    m = len(matrix_b[0])
+    result = [[0 for _ in range(m)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            for k in range(len(matrix_b)):
+                result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+
+    return result
+
 
 def functions(a_1, a_2):
     """
@@ -19,6 +33,60 @@ def functions(a_1, a_2):
     """
     # put your code here
     pass
+    
+    coeffs1 = [int(coeff) for coeff in a_1.split()]
+    coeffs2 = [int(coeff) for coeff in a_2.split()]
+
+    # находим точки экстремума:
+    extreme_point_1 = get_extreme(coeffs1)
+    extreme_point_2 = get_extreme(coeffs2)
+    print(extreme_point_1)
+    print(extreme_point_2)
+
+    common_x = find_common_x(coeffs1, coeffs2)
+    if common_x is None:
+        return None
+    return [(x, get_y(x, coeffs1)) for x in common_x]
+
+
+def get_extreme(coeffs):
+    a, b, c = coeffs
+
+    if a == 0:
+        return None
+    x_extreme = -b / (2 * a)
+    y_extreme = a * x_extreme * x_extreme + b * x_extreme + c
+    return x_extreme, y_extreme
+
+
+def find_common_x(coeffs1, coeffs2):
+    a = coeffs1[0] - coeffs2[0]
+    b = coeffs1[1] - coeffs2[1]
+    c = coeffs1[2] - coeffs2[2]
+
+    if a == b == 0:
+        if c == 0:
+            return None
+        return []
+
+    if a == 0:
+        x = -c / b
+        return [x]
+
+    D = b * b - 4 * a * c
+    if D < 0:
+        return []
+    elif D == 0:
+        x = -b / (2 * a)
+        return [x]
+    else:
+        x1 = (-b + D ** 0.5) / (2 * a)
+        x2 = (-b - D ** 0.5) / (2 * a)
+        return [x1, x2]
+
+
+def get_y(x, coeffs):
+    return sum(x ** i * coeffs[::-1][i] for i in range(len(coeffs)))
 
 
 def skew(x):
@@ -29,6 +97,9 @@ def skew(x):
     # put your code here
     pass
 
+    skew = np.mean((x - np.mean(x)) ** 3) / np.std(x) ** 3
+    return round(skew, 2)
+
 
 def kurtosis(x):
     """
@@ -37,3 +108,6 @@ def kurtosis(x):
     """
     # put your code here
     pass
+
+    kurtosis = np.mean((x - np.mean(x)) ** 4) / np.std(x) ** 4 - 3
+    return round(kurtosis, 2)
